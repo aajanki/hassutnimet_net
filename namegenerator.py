43b88116a6c2843ffadc4d40e6970f44eeeb1b35
Antti Ajanki <antti.ajanki@iki.fi>
2017-07-20 20:07:27 +0300

    Simple name generator

---
+++ namegenerator.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import csv
+import random
+import sys
+
+
+word_start = '^'
+word_end = '$'
+
+
+class Markov(object):
+    def __init__(self, n, statistics):
+        self.n = n
+        self.statistics = statistics
+
+
+def ngrams(n, word):
+    for i in range(len(word) - n + 1):
+        yield word[i:i+n]
+
+
+def unzip(list_of_pairs):
+    return [list(t) for t in zip(*list_of_pairs)]
+
+
+def collect_statistics(rows, n):
+    word_prefix = word_start * (n-1)
+    
+    names = []
+    samples = {}
+    for row in rows:
+        word = row['Name'].lower()
+        names.append(word)
+        count = int(float(row['Count']))
+        prepared_word = word_prefix + word + word_end
+        for ngram in ngrams(n, prepared_word):
+            prefix = ngram[:-1]
+            successor = ngram[-1]
+
+            counts = samples.get(prefix, {})
+            counts[successor] = counts.get(successor, 0) + count
+            samples[prefix] = counts
+
+    markov_weights = {}
+    for prefix, values in samples.items():
+        markov_weights[prefix] = tuple(unzip(values.items()))
+        
+    return Markov(n, markov_weights)
+
+
+def markov_sample(markov):
+    context = (markov.n - 1) * word_start
+
+    sample = []
+    while True:
+        successors, weights = markov.statistics[context]
+        next_letter = random.choices(successors, weights)[0]
+        if next_letter == word_end:
+            break
+
+        sample.append(next_letter)
+        context = context[1:] + next_letter
+        
+    return ''.join(sample)
+
+
+def read_data(csvfilename):
+    with open(csvfilename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            yield row
+
+
+def main():
+    markov = collect_statistics(read_data(sys.argv[1]), 4)
+
+    for _ in range(20):
+        print(markov_sample(markov))
+
+
+if __name__ == '__main__':
+    main()
