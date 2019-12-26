7cd27f1f4493d6eaa87d9d96646284486f9d9a28
Nicolas Kyejo <nicolas.kyejo@metropolia.fi>
2019-12-26 20:45:51 +0200

    forgot 2019... in if condition, works without it though

--- scripts/get_vrk_data.sh
+++ scripts/get_vrk_data.sh
@@ -3,7 +3,7 @@
 if [ ! -f data/raw/etunimitilasto-2019-08-07-vrk.xlsx ]; then
    mkdir -p data/raw && wget https://www.avoindata.fi/data/dataset/57282ad6-3ab1-48fb-983a-8aba5ff8d29a/resource/46f4f0b1-dc1f-4c28-a23b-2fba6cfa19b4/download/etunimitilasto-2019-08-07-vrk.xlsx -O data/raw/etunimitilasto-2019-08-07-vrk.xlsx
 fi
-if [ ! -f data/raw/Sukunimitilasto-2017-04-06-VRK.xls ]; then
+if [ ! -f data/raw/Sukunimitilasto-2019-08-07-vrk.xlsx ]; then
    wget https://www.avoindata.fi/data/dataset/57282ad6-3ab1-48fb-983a-8aba5ff8d29a/resource/a32aa571-23aa-4bfc-ba72-106f74c5252e/download/sukunimitilasto-2019-08-07-vrk.xlsx -O data/raw/sukunimitilasto-2019-08-07-vrk.xlsx
 fi
 
